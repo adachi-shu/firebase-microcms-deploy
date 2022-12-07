2b176baedf3403b61c399ffd4a4c17208a2a55e1
adachi-shu <backnumber0620@icloud.com>
2022-12-07 14:03:48 +0900

    develop first commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          自動デプロイ
         </h1>
 
         <p className={styles.description}>
