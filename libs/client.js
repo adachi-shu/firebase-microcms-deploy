0c8e06971e71e4bc6c117bebe52c534e2aae433a
adachi-shu <backnumber0620@icloud.com>
2022-12-07 18:15:32 +0900

    jikagaki

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'fire',
-  apiKey: process.env.API_KEY,
+  apiKey: 'lFkOD4SDnAsNOgH3HyJWcAikNj1ZV4lqQp6S',
 });
